6f178eb8dc32a60287df7cbf70f57d9f6fc17afd
Md. Saiedur Rahman Saied <saied@pop-os.Dlink>
2024-07-09 15:06:09 +0600

    design navbar: :

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
+    <div className="bg-slate-50">
       <Navbar />
-    </>
+    </div>
   );
 };
 
