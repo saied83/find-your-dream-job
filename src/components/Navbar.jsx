a296c96129cab56d8a6f9ed5f45d8f966757188b
saied83 <saiedtechit@gmail.com>
2024-07-12 16:33:29 +0600

    fetch data using proxy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,12 +13,12 @@ const Navbar = () => {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-              <a className="flex flex-shrink-0 items-center mr-4" to="/">
+              <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
                 <img className="h-10 w-auto" src={logo} alt="React Jobs" />
                 <span className="hidden md:block text-white text-2xl font-bold ml-2">
                   Job Hunter
                 </span>
-              </a>
+              </NavLink>
               <div className="md:ml-auto">
                 <div className="flex space-x-2">
                   <NavLink to="/" className={linkClass}>
