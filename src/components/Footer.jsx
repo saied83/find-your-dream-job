cca18d77d836161195d54df1fcfc9dd4b39021c6
saied83 <saiedtechit@gmail.com>
2024-07-17 18:34:26 +0600

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="h-16 text-white flex justify-center items-center text-lg w-full bg-indigo-700 border-t border-indigo-500">
+      All right reserved @find-your-dream-job
+    </div>
+  );
+};
+
+export default Footer;
